8201fae914daef28c7364e8324926f5187bca4df
nataliamu <56892635+nataliamu@users.noreply.github.com>
2019-10-23 15:49:00 +0200

    Update zadanie6_konwersja.py

--- zadanie6_konwersja.py
+++ zadanie6_konwersja.py
@@ -7,3 +7,13 @@ for file in os.listdir(path):
             file1 = file[0:-3] + 'png'
             print(file)
             os.rename(file, file1)
+            
+'''
+import os
+files = os.listdir()
+for file in files:
+    name, ext = file.split('.')
+    if ext == 'jpg':
+        os.rename(file,name + ".png")
+        
+print(files)'''

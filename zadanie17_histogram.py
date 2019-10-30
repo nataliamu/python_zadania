66744ba9ce1ac2125603cbcf91b8be76a1b16a82
nataliamu <56892635+nataliamu@users.noreply.github.com>
2019-10-30 16:39:47 +0100

    Update zadanie17_histogram.py

--- zadanie17_histogram.py
+++ zadanie17_histogram.py
@@ -70,3 +70,59 @@ def create_histogram(size):
 p = Pool()
 hist = p.map(create_histogram, (500000,) )
 '''
+
+
+
+'''
+import random
+import time
+import multiprocessing
+
+
+array = [ random.randint(0, 10) for i in range(1000000) ]
+
+
+def count(data):
+    result = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0}
+    for i in data:
+        result[i] += 1
+    return result
+
+start_1 = time.perf_counter()
+result_1 = count(array)
+hist_time_1 = time.perf_counter() - start_1
+print(hist_time_1, ' -> ', result_1)
+
+
+# ----------------------------------------------------
+import multiprocessing
+
+if __name__ == '__main__':
+    array = [ random.randint(0, 10) for i in range(1000000) ]
+
+
+    def process_worker(name, data, process_result):
+        result = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0}
+        for i in data:
+            result[i] += 1
+        process_result[name] = result
+
+    manager = multiprocessing.Manager()
+    process_result = manager.dict()
+    jobs = []
+
+    p1 = multiprocessing.Process(target=process_worker, args=(1, array[:200000], process_result))
+    p2 = multiprocessing.Process(target=process_worker, args=(2, array[200000:400000], process_result))
+    p3 = multiprocessing.Process(target=process_worker, args=(3, array[400000:600000], process_result))
+    p4 = multiprocessing.Process(target=process_worker, args=(4, array[600000:800000], process_result))
+    p5 = multiprocessing.Process(target=process_worker, args=(5, array[800000:], process_result))
+    jobs.append(p1)
+    jobs.append(p2)
+    jobs.append(p3)
+    jobs.append(p4)
+    jobs.append(p5)
+
+    for p in jobs: p.start()
+    for p in jobs: p.join()
+    print(process_result.values())
+    '''
